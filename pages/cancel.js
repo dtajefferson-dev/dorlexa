dfea91ff920d901abaf716fed1b6c9150c5d9b2e
Dta <dtajefferson@gmail.com>
2026-02-15 18:36:21 -0800

    Delete pages directory

--- pages/cancel.js
+++
@@ -1,3 +0,0 @@
-export default function Cancel() {
-  return <div style={{ padding: '3rem', textAlign: 'center' }}><h1>Call cancelled</h1></div>;
-}
--- pages/success.js
+++
@@ -1,3 +0,0 @@
-export default function Success() {
-  return <div style={{ padding: '3rem', textAlign: 'center' }}><h1>Payment received! Call connecting…</h1></div>;
-}
